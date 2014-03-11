29c8240632df1e5a9650009abcf0a6825abb04e8
GuillaumeOcculy <occuly.guillaume@gmail.com>
2014-03-11 22:27:09 +0100

    Genfile & secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Twitter::Application.config.secret_key_base = 'd54c4eca71e498001024c6e19f758d9581aacfd2f456d5aee700e82d7d56ba5307fe3604df974a8e51428fb26d887e0e3bbdf233b82826540024fadd4c45ee0e'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
